f58c34e0f993704507fe2890dcae1a494be062e4
Samdanova <samdanova123@gmail.com>
2023-12-10 19:51:15 +0200

    adding and removing

--- tree/src/components/tree.tsx
+++ tree/src/components/tree.tsx
@@ -1,99 +1,104 @@
-import React, {useState, useContext} from 'react';
+import React, {useState, useContext, useEffect} from 'react';
+import {default_list_values} from '../constant/constant'
 
 export interface Node {
     id: number;
     name: string;
     items?: Node[];
-  }
+    onDelete?: (id:number)=>void;
+}
 
-export default function Tree(props:Node):JSX.Element {
-    const {
-        id,
-        name,
-        items,
-      } = props;
+export default function Tree(props : Node): JSX.Element {
+    const {id, name, items, onDelete} = props;
+    // let copyListValues = [...default_list_values];
 
-      const [state, setState] = useState(props)
-      const[pressedEdit,setEdit]=useState(false);
-      const[pressedSave,setSave]=useState(false);
-   const handleEdit = () => {
+    //states
+    const [nodes, setNodes] = useState(items || [])
+    const [stateProps, setState] = useState({id, name, items})
+    const [pressedEdit, setEdit] = useState(false);
+    const [pressedSave, setSave] = useState(false);
+
+    //functions
+    const handleEdit = () => { //editing
         setEdit(!pressedEdit);
         setSave(!pressedSave);
     }
 
-    const handleSave = () => {
-      setEdit(!pressedEdit);
-      setSave(!pressedSave);
+    const handleSave = () => { //saving changes
+        setEdit(!pressedEdit);
+        setSave(!pressedSave);
     }
-    const handleChange=(event: React.ChangeEvent<HTMLInputElement>):void=>{
-setState({
-  ...state,
-  name: event.target.value, 
-})
+    const handleChange = (event : React.ChangeEvent<HTMLInputElement>) : void => { //catch changes
+        setState({
+            ...stateProps,
+            name: event.target.value
+        })
     }
- const handleReset=()=>{
-setState(props)
- }
+    const handleReset = () => { //reset all changes
+        setState({id,name,items})
+    }
 
- const handleAdd=()=>{
-  const newElement: Node = {
-    id: Math.floor(Math.random() * 1000),
-    name: `New Element`,
-  };
-  addElement(id, newElement); // Здесь 1 - id родительского элемента, к которому добавляем новый элемент
-   }
-
-   const addElement = (parentId: number, newElement: Node) => {
-      if (id === parentId) {
-        return {
-          ...state,  
-          items: items ? [...items, newElement] : [newElement],
+    const handleAdd = () => { //adding element
+        const newElement: Node = {
+            id: Math.floor(Math.random() * 1000),
+            name: `New Element`
         };
-      }
-      if (items){
-        return{
-          ...state,
-          items: addElement(parentId, newElement, items),
-        };
-      }
-    setState(updatedTreeData);
-  };
+        setNodes([
+            ...nodes,
+            newElement
+        ])
+    }
 
+    
 
-
-   const handleRemove=()=>{
-    setState(props)
-     }
-
-      const treeItems = items?.map((node) => {
-        if (node.items !== null) {
-          return (
-            <Tree 
-              id={node.id} 
-              key={node.id} 
-              name={node.name} 
-              items={node.items}  />
-          )
-        }
-      });
+    const handleRemove = (id:number) => { //removing
+      const filteredItems = nodes.filter((item) => id !== item.id);
+      setNodes(filteredItems);
+      
+    }
+    //painting tree elements
+  const treeItems = nodes
+        ?.map((node) =>  {
+            if (node.items !== null) {
+                return (< Tree id = {
+                    node.id
+                }
+                key = {
+                    node.id
+                }
+                name = {
+                    node.name
+                }
+                items = {
+                    node.items
+                } 
+                onDelete={handleRemove}/>)
+            } 
+        });
     return (
-<div>
-    <li>
-        <div>{ pressedEdit?
-            <input value={state.name} onChange={handleChange}/>
-            :state.name} 
-            { pressedEdit?
-            <button onClick={handleSave}>Save</button>
-            :
-            <button onClick={handleEdit}>Edit</button>}
-            <button onClick={handleRemove}>Remove</button>
-            <button onClick={handleAdd}>Add</button>
-            <button onClick={handleReset}>Reset</button>
-        </div>
-        <ul>
-            {treeItems}
-        </ul>
-    </li>
-</div>
+        <div><li><div> {
+            pressedEdit
+                ? <input value = {
+                    stateProps.name
+                }
+                onChange = {
+                    handleChange
+                } />
+                : stateProps.name
+        } {
+            pressedEdit
+                ? <button onClick = {
+                    handleSave
+                } > Save</button>
+                : <button onClick = {
+                    handleEdit
+                } > Edit</button>
+        } < button onClick = {()=>onDelete && onDelete(id)}>Remove</button> < button onClick = {
+            handleAdd
+        } > Add</button> < button onClick = {
+            handleReset
+        } > Reset</button></div><ul> {
+            treeItems
+        }</ul></li></div>
     )
 }
